19ef809cb9359abb6d4ba4ef41deb4412c9b49b4
Ishita Dasgupta <ishitadg@cs.umass.edu>
2018-06-19 18:13:57 -0400

    Update 1_setup.sh

--- 1_setup.sh
+++ 1_setup.sh
@@ -11,21 +11,21 @@ sudo apt-get install docker-ce
 ##Install Ovs-docker utility
 sudo apt-get install openvswitch-switch
 cd /usr/bin
-wget -L https://raw.githubusercontent.com/openvswitch/ovs/master/utilities/ovs-docker
-chmod a+rwx ovs-docker
+sudo wget -L https://raw.githubusercontent.com/openvswitch/ovs/master/utilities/ovs-docker
+sudo chmod a+rwx ovs-docker
 
 ##Create an ovs bridge: Here we will be adding a new OVS bridge and configuring it, 
 #so that we can get the containers connected on the different network.
-ovs-vsctl add-br ovs-br1
-ifconfig ovs-br1 173.16.1.1 netmask 255.255.255.0 up
+sudo ovs-vsctl add-br ovs-br1
+sudo ifconfig ovs-br1 173.16.1.1 netmask 255.255.255.0 up
 
 ##Add a port from OVS bridge to the Docker Container
 #create containers
-docker run -t -i --name container2 ubuntu /bin/bash
-docker run -t -i --name container2 ubuntu /bin/bash
+sudo docker run -t -i --name container2 ubuntu /bin/bash
+sudo docker run -t -i --name container2 ubuntu /bin/bash
 #connect them to ovs-bridge
-ovs-docker add-port ovs-br1 eth1 container1 --ipaddress=173.16.1.2/24
-ovs-docker add-port ovs-br1 eth1 container2 --ipaddress=173.16.1.3/24
+sudo ovs-docker add-port ovs-br1 eth1 container1 --ipaddress=173.16.1.2/24
+sudo ovs-docker add-port ovs-br1 eth1 container2 --ipaddress=173.16.1.3/24
 #Test the connection between two containers connected via OVS bridge using Ping command
 
 echo done
