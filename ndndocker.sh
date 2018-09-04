d7dc9f890709388b2d4652f78c7091edb7f87935
Ishita Dasgupta <ishitadg@cs.umass.edu>
2018-09-04 03:26:39 -0400

    Update ndndocker.sh

--- ndndocker.sh
+++ ndndocker.sh
@@ -8,4 +8,4 @@ mv Dockerfile?token=AKYyYp0eCb26g9Eu9X9bjYK2rJ7oRcpuks5bl2ppwA%3D%3D Dockerfile
 docker build -t ndndock --build-arg VERSION_CXX=ndn-cxx-0.6.1 --build-arg VERSION_NFD=NFD-0.6.1 .
 
 #Run NDN docker
-docker run -d --rm --name ndn1 ndndock
+docker run -d --rm --name ndn1 -p 6364:6363 -p 6365:6363/udp ndndock
