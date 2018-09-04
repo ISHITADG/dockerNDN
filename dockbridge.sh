dcb1165f75cf1e2d049a6031c69327baf50a7ec2
Ishita Dasgupta <ishitadg@cs.umass.edu>
2018-09-04 04:11:57 -0400

    Update dockbridge.sh

--- dockbridge.sh
+++ dockbridge.sh
@@ -6,13 +6,9 @@ sudo ifconfig ovs-br1 173.16.1.1 netmask 255.255.255.0 up
 sudo ovs-vsctl show
 
 ##configure this bridge with some container - in our case NDN Docker and IP Docker
-#configuring NDN Docker
-sudo docker run -t -i --name container1 ubuntu /bin/bash
-#configuring IP Docker
-sudo docker run -t -i --name container2 ubuntu /bin/bash
-#connect the above containers to the OVS bridge ovs-br1
-sudo ovs-docker add-port ovs-br1 eth1 container1 --ipaddress=173.16.1.2/24
-sudo ovs-docker add-port ovs-br1 eth1 container2 --ipaddress=173.16.1.3/24
+#connect the given containers to the OVS bridge ovs-br1
+sudo ovs-docker add-port ovs-br1 eth1 ip1 --ipaddress=173.16.1.2/24
+sudo ovs-docker add-port ovs-br1 eth1 ndn1 --ipaddress=173.16.1.3/24
 
 ##adding extra bridges- eth1 
 #sudo ovs-vsctl add-port ovs-br1 eth1
