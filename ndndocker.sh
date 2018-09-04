b665bd71963a429c929944a7e58ad8e5ceefbceb
Ishita Dasgupta <ishitadg@cs.umass.edu>
2018-09-04 03:16:07 -0400

    Build NDN Docker

---
+++ ndndocker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#download NDN Dockerfile 
+wget -L https://raw.githubusercontent.com/ISHITADG/dockerNDN/master/Dockerfile?token=AKYyYp0eCb26g9Eu9X9bjYK2rJ7oRcpuks5bl2ppwA%3D%3D
+mv Dockerfile?token=AKYyYp0eCb26g9Eu9X9bjYK2rJ7oRcpuks5bl2ppwA%3D%3D Dockerfile
+
+#build docker ndn docker type
+docker build -t ndndock --build-arg VERSION_CXX=ndn-cxx-0.6.1 --build-arg VERSION_NFD=NFD-0.6.1 .
