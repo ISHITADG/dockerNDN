a7614b18bcb44ecf37e341336c931a155c95ba94
Ishita Dasgupta <ishitadg@cs.umass.edu>
2018-10-03 13:22:47 -0400

    Update dockovs.sh

--- dockovs.sh
+++ dockovs.sh
@@ -13,7 +13,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install docker-ce
-sudo cd /usr/bin
+cd /usr/bin
+sudo rm ovs-docker
 sudo wget https://raw.githubusercontent.com/openvswitch/ovs/master/utilities/ovs-docker
 sudo chmod a+rwx ovs-docker
 
