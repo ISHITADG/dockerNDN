d410ca486e7719ef2c26a2a6dd98ef281c1d4fa8
Ishita Dasgupta <ishitadg@cs.umass.edu>
2020-05-15 10:03:21 -0400

    added -y

--- dockovs.sh
+++ dockovs.sh
@@ -2,17 +2,17 @@
 
 ##Install OVS
 sudo apt-get update
-sudo apt-get install openvswitch-switch
+sudo apt-get install -y openvswitch-switch
 
 sudo ovs-vsctl --version
 
 ##install docker-ce
 sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
 sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get install -y docker-ce
 cd /usr/bin
 sudo rm ovs-docker
 sudo wget https://raw.githubusercontent.com/openvswitch/ovs/master/utilities/ovs-docker
